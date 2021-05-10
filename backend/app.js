6d86102ec5b19e27734cca74aaef7275a6e65d35
lucie-gonzalez <76063929+lucie-gonzalez@users.noreply.github.com>
2021-05-10 14:59:09 +0200

    améliorations

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
+const MaskData = require('maskdata');
+const helmet = require("helmet");
+require('dotenv').config();
+
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 
@@ -10,7 +14,24 @@ mongoose.connect('mongodb+srv://openclassroom:imth3dStWaHpVc3@sauce.3plzk.mongod
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-  app.use('/api/sauces', saucesRoutes);
+ 
+
+// création de l'application express
+const app=express();
+
+const rateLimit = require("express-rate-limit");
+
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+//  apply to all requests
+app.use(limiter);
+
+//helmet securité
+app.use(helmet());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,7 +39,13 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+// transformation du corps des requêtes en objets javascript utilisables
 app.use(bodyParser.json());
+
+// gestion de la ressource images de manière statique
+app.use('/images', express.static(path.join( __dirname, 'images')));
+
+// enregistrement des routeurs sauce et utilisateur pour n'importe quelle requête effectuée vers /api/sauces et /api/auth
 app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,25 +2,35 @@
 const bcrypt = require('bcrypt');
 
 const jwt = require('jsonwebtoken');
-
+const MaskData = require('maskdata');
 const user = require('../models/user');
 
+//masquage e-mail
+const emailMask2Options = {
+  maskWith: "*", 
+  unmaskedStartCharactersBeforeAt: 5,
+  unmaskedEndCharactersAfterAt: 5,
+  maskAtTheRate: false
+};
+
 exports.signup = (req, res, next) => {
+  const maskedEmail = MaskData.maskEmail2(req.body.email, emailMask2Options);
     bcrypt.hash(req.body.password, 10)
       .then(hash => {
         const user = new User({
-          email: req.body.email,
+          email: maskedEmail,
           password: hash
         });
         user.save()
           .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          .catch(error => res.status(400).json({ error }));
+          .catch(error => res.status(400).json({ error:error.message }));
       })
       .catch(error => res.status(500).json({ error }));
   };
 
   exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+    const maskedEmail = MaskData.maskEmail2(req.body.email, emailMask2Options);
+    User.findOne({ email: maskedEmail })
       .then(user => {
         if (!user) {
           return res.status(401).json({ error: 'Utilisateur non trouvé !' });
@@ -34,7 +44,7 @@ exports.signup = (req, res, next) => {
               userId: user._id,
               token: jwt.sign(
                 { userId: user._id },
-                'RANDOM_TOKEN_SECRET',
+                'TOKEN',
                 { expiresIn: '24h' }
                 )
               });
